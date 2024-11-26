87cf229a91f02434596bec1255fb7571b7911c33
Lorena Suarez <lorenandreasuarez@gmail.com>
2024-11-26 16:25:13 -0300

    actualizacion conection render

--- src/components/MiComponente.jsx
+++ src/components/MiComponente.jsx
@@ -1,23 +1,26 @@
-// src/components/MiComponente.jsx
-import React from 'react';
-import { useCart } from '../context/CartContext'; // Asegúrate de que la ruta sea correcta
+import React, { useEffect, useState } from "react";
 
 const MiComponente = () => {
-    const { carrito, agregarCarrito, calcularTotal } = useCart();
+  const [data, setData] = useState(null);
+  const apiUrl = process.env.REACT_APP_API_URL;
 
-    return (
-        <div>
-            <h2>Total: {calcularTotal()}</h2>
-            <ul>
-                {carrito.map(item => (
-                    <li key={item.id_producto}>
-                        {item.nombre} - Cantidad: {item.count} - Precio: {item.precio}
-                    </li>
-                ))}
-            </ul>
-            {/* Aquí puedes agregar más lógica para mostrar los productos en el carrito */}
-        </div>
-    );
+  useEffect(() => {
+    fetch(`${apiUrl}/api/productos`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error en la respuesta de la API");
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error:", error));
+  }, [apiUrl]); // La dependencia es apiUrl
+
+  return (
+    <div>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Cargando...</p>}
+    </div>
+  );
 };
 
 export default MiComponente;
